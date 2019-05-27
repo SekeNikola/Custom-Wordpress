122398421013d9d30d2257eacf06e39bab86caa5
Seke Nikola <sekenikola@gmail.com>
2019-05-27 13:38:42 +0200

    add more search functionality

--- html/wp-content/themes/univeristy/js/modules/Search.js
+++ html/wp-content/themes/univeristy/js/modules/Search.js
@@ -1,49 +1,52 @@
 function Search() {
-  const openButton = document.getElementById('search');
-  const closeButton = document.getElementById('close-overlay');
-  const searchOverlay = document.getElementById('overlay');
+  const openButton = document.getElementById("search");
+  const closeButton = document.getElementById("close-overlay");
+  const searchOverlay = document.getElementById("overlay");
   const body = document.body;
-  const searchTerm = document.getElementById('search-term');
+  const searchTerm = document.getElementById("search-term");
   let typingTimeout;
-  const resultsDiv = document.getElementById('search-overlay__results');
+  const resultsDiv = document.getElementById("search-overlay__results");
   let isSpinnerVisible = false;
   let previusValue;
 
-  openButton.addEventListener('click', function () {
-    searchOverlay.classList.add('search-overlay--active')
-    body.classList.add('body-no-scroll')
-
+  openButton.addEventListener("click", function() {
+    searchOverlay.classList.add("search-overlay--active");
+    body.classList.add("body-no-scroll");
   });
-  closeButton.addEventListener('click', function () {
-    searchOverlay.classList.remove('search-overlay--active')
-    body.classList.remove('body-no-scroll')
+  closeButton.addEventListener("click", function() {
+    searchOverlay.classList.remove("search-overlay--active");
+    body.classList.remove("body-no-scroll");
   });
 
-  searchTerm.addEventListener('keyup', function () {
+  searchTerm.addEventListener("keyup", function() {
     if (searchTerm.value != previusValue) {
       clearTimeout(typingTimeout);
       if (searchTerm.value) {
         if (!isSpinnerVisible) {
-          let div = document.createElement('div');
-          div.className = 'spinner-loader';
-          resultsDiv.appendChild(div)
-          isSpinnerVisible = true
+          let div = document.createElement("div");
+          div.className = "spinner-loader";
+          resultsDiv.appendChild(div);
+          isSpinnerVisible = true;
         }
-        typingTimeout = setTimeout(function () {
-          resultsDiv.textContent = 'This ios the result'
-          console.log(searchTerm.value);
+        typingTimeout = setTimeout(function() {
+          let url = `http://localhost:8000/wp-json/wp/v2/posts?search=${
+            searchTerm.value
+          }`;
+          fetch(url)
+            .then(res => res.json())
+            .then(out => {
+              alert(out[0].title.rendered);
+            });
 
-          isSpinnerVisible = false
+          isSpinnerVisible = false;
         }, 2000);
       } else {
-        resultsDiv.innerHTML = ''
-        isSpinnerVisible = false
+        resultsDiv.innerHTML = "";
+        isSpinnerVisible = false;
       }
     }
-    previusValue = searchTerm.value
-  })
+    previusValue = searchTerm.value;
+  });
 }
 
-
-
-export default Search
+export default Search;
